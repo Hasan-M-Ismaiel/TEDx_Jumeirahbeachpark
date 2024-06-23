2e891cb2ce0dcf1f24d699b7284695f70486fafc
hasan <hasanismaiel19951995@gmail.com>
2024-06-23 08:19:03 +0300

    first commit

---
+++ resources/views/includes/members.blade.php
@@ -0,0 +1,205 @@
+<section id="members" class="pricing-table section overlay">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="section-title white-title">
+                    <h3 class="wow zoomIn" data-wow-delay=".2s">members</h3>
+                    <h2 class="wow fadeInUp" data-wow-delay=".4s">Meet our Heros</h2>
+                    <p class="wow fadeInUp" data-wow-delay=".6s">The TEDxJumeirah experience is brought to life by a dedicated team of passionate individuals who believe in the power of ideas. Our team members come from diverse backgrounds and share a common drive to create a platform for inspiring conversations.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="container">
+                <div class="all-speakers">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-4 col-12 wow fadeInUp" data-wow-delay=".2s">
+                            <!-- Start Single Speaker -->
+                            <div class="single-speaker">
+                                <div class="top-content">
+                                    <div class="image">
+                                        <img src="{{ asset('assets_main/assets/images/speakers/speaker1.png') }}" alt="#">
+                                    </div>
+                                    <ul class="social">
+                                        <li class="facebook"><a href="javascript:void(0)"><i class="lni lni-facebook-filled"></i></a></li>
+                                        <li class="twitter"><a href="javascript:void(0)"><i class="lni lni-twitter-filled"></i></a></li>
+                                        <li class="linkedin"><a href="javascript:void(0)"><i class="lni lni-linkedin-original"></i></a>
+                                        </li>
+                                        <li class="instagram"><a href="javascript:void(0)"><i class="lni lni-instagram-original"></i></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="name">
+                                    <h3><a href="javascript:void(0)">Fredric Martinsson</a></h3>
+                                    <span>Founder, Edilta</span>
+                                </div>
+                            </div>
+                            <!-- End Single Speaker -->
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-12 wow fadeInUp" data-wow-delay=".4s">
+                            <!-- Start Single Speaker -->
+                            <div class="single-speaker">
+                                <div class="top-content">
+                                    <div class="image">
+                                        <img src="{{ asset('assets_main/assets/images/speakers/speaker2.png') }}" alt="#">
+                                    </div>
+                                    <ul class="social">
+                                        <li class="facebook"><a href="javascript:void(0)"><i class="lni lni-facebook-filled"></i></a></li>
+                                        <li class="twitter"><a href="javascript:void(0)"><i class="lni lni-twitter-filled"></i></a></li>
+                                        <li class="linkedin"><a href="javascript:void(0)"><i class="lni lni-linkedin-original"></i></a>
+                                        </li>
+                                        <li class="instagram"><a href="javascript:void(0)"><i class="lni lni-instagram-original"></i></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="name">
+                                    <h3><a href="javascript:void(0)">Melisa Lundryn</a></h3>
+                                    <span>Lead Designer</span>
+                                </div>
+                            </div>
+                            <!-- End Single Speaker -->
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-12 wow fadeInUp" data-wow-delay=".6s">
+                            <!-- Start Single Speaker -->
+                            <div class="single-speaker">
+                                <div class="top-content">
+                                    <div class="image">
+                                        <img src="{{ asset('assets_main/assets/images/speakers/speaker3.png') }}" alt="#">
+                                    </div>
+                                    <ul class="social">
+                                        <li class="facebook"><a href="javascript:void(0)"><i class="lni lni-facebook-filled"></i></a></li>
+                                        <li class="twitter"><a href="javascript:void(0)"><i class="lni lni-twitter-filled"></i></a></li>
+                                        <li class="linkedin"><a href="javascript:void(0)"><i class="lni lni-linkedin-original"></i></a>
+                                        </li>
+                                        <li class="instagram"><a href="javascript:void(0)"><i class="lni lni-instagram-original"></i></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="name">
+                                    <h3><a href="javascript:void(0)">Andrio Limuya</a></h3>
+                                    <span>Web Developer</span>
+                                </div>
+                            </div>
+                            <!-- End Single Speaker -->
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-12 wow fadeInUp" data-wow-delay=".8s">
+                            <!-- Start Single Speaker -->
+                            <div class="single-speaker">
+                                <div class="top-content">
+                                    <div class="image">
+                                        <img src="{{ asset('assets_main/assets/images/speakers/speaker4.png') }}" alt="#">
+                                    </div>
+                                    <ul class="social">
+                                        <li class="facebook"><a href="javascript:void(0)"><i class="lni lni-facebook-filled"></i></a></li>
+                                        <li class="twitter"><a href="javascript:void(0)"><i class="lni lni-twitter-filled"></i></a></li>
+                                        <li class="linkedin"><a href="javascript:void(0)"><i class="lni lni-linkedin-original"></i></a>
+                                        </li>
+                                        <li class="instagram"><a href="javascript:void(0)"><i class="lni lni-instagram-original"></i></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="name">
+                                    <h3><a href="javascript:void(0)">Agaton Johnsson</a></h3>
+                                    <span>Expert Developer</span>
+                                </div>
+                            </div>
+                            <!-- End Single Speaker -->
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-12 wow fadeInUp" data-wow-delay=".2s">
+                            <!-- Start Single Speaker -->
+                            <div class="single-speaker">
+                                <div class="top-content">
+                                    <div class="image">
+                                        <img src="{{ asset('assets_main/assets/images/speakers/speaker5.png') }}" alt="#">
+                                    </div>
+                                    <ul class="social">
+                                        <li class="facebook"><a href="javascript:void(0)"><i class="lni lni-facebook-filled"></i></a></li>
+                                        <li class="twitter"><a href="javascript:void(0)"><i class="lni lni-twitter-filled"></i></a></li>
+                                        <li class="linkedin"><a href="javascript:void(0)"><i class="lni lni-linkedin-original"></i></a>
+                                        </li>
+                                        <li class="instagram"><a href="javascript:void(0)"><i class="lni lni-instagram-original"></i></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="name">
+                                    <h3><a href="javascript:void(0)">Rebecca Henrikon</a></h3>
+                                    <span>Founder, Cards</span>
+                                </div>
+                            </div>
+                            <!-- End Single Speaker -->
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-12 wow fadeInUp" data-wow-delay=".4s">
+                            <!-- Start Single Speaker -->
+                            <div class="single-speaker">
+                                <div class="top-content">
+                                    <div class="image">
+                                        <img src="{{ asset('assets_main/assets/images/speakers/speaker6.png') }}" alt="#">
+                                    </div>
+                                    <ul class="social">
+                                        <li class="facebook"><a href="javascript:void(0)"><i class="lni lni-facebook-filled"></i></a></li>
+                                        <li class="twitter"><a href="javascript:void(0)"><i class="lni lni-twitter-filled"></i></a></li>
+                                        <li class="linkedin"><a href="javascript:void(0)"><i class="lni lni-linkedin-original"></i></a>
+                                        </li>
+                                        <li class="instagram"><a href="javascript:void(0)"><i class="lni lni-instagram-original"></i></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="name">
+                                    <h3><a href="javascript:void(0)">Lundryn Melisa</a></h3>
+                                    <span>Graphics Designer</span>
+                                </div>
+                            </div>
+                            <!-- End Single Speaker -->
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-12 wow fadeInUp" data-wow-delay=".6s">
+                            <!-- Start Single Speaker -->
+                            <div class="single-speaker">
+                                <div class="top-content">
+                                    <div class="image">
+                                        <img src="{{ asset('assets_main/assets/images/speakers/speaker7.png') }}" alt="#">
+                                    </div>
+                                    <ul class="social">
+                                        <li class="facebook"><a href="javascript:void(0)"><i class="lni lni-facebook-filled"></i></a></li>
+                                        <li class="twitter"><a href="javascript:void(0)"><i class="lni lni-twitter-filled"></i></a></li>
+                                        <li class="linkedin"><a href="javascript:void(0)"><i class="lni lni-linkedin-original"></i></a>
+                                        </li>
+                                        <li class="instagram"><a href="javascript:void(0)"><i class="lni lni-instagram-original"></i></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="name">
+                                    <h3><a href="javascript:void(0)">Julia Milon</a></h3>
+                                    <span>Ui/Ux Designer</span>
+                                </div>
+                            </div>
+                            <!-- End Single Speaker -->
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-12 wow fadeInUp" data-wow-delay=".8s">
+                            <!-- Start Single Speaker -->
+                            <div class="single-speaker">
+                                <div class="top-content">
+                                    <div class="image">
+                                        <img src="{{ asset('assets_main/assets/images/speakers/speaker8.png') }}" alt="#">
+                                    </div>
+                                    <ul class="social">
+                                        <li class="facebook"><a href="javascript:void(0)"><i class="lni lni-facebook-filled"></i></a></li>
+                                        <li class="twitter"><a href="javascript:void(0)"><i class="lni lni-twitter-filled"></i></a></li>
+                                        <li class="linkedin"><a href="javascript:void(0)"><i class="lni lni-linkedin-original"></i></a>
+                                        </li>
+                                        <li class="instagram"><a href="javascript:void(0)"><i class="lni lni-instagram-original"></i></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="name">
+                                    <h3><a href="javascript:void(0)">Digolia Sujiyan</a></h3>
+                                    <span>Founder, Edilta</span>
+                                </div>
+                            </div>
+                            <!-- End Single Speaker -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
